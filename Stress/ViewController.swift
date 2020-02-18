ae0fd80272ed5300e6158a85ba4eae82530474d4
Shawn Koh <shawnkoh@me.com>
2020-02-19 02:24:44 +0800

    Initial commit

---
+++ Stress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stress
+//
+//  Created by Shawn Koh on 19/2/20.
+//  Copyright © 2020 Shawn Koh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
