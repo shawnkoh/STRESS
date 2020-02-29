eb203489cbc1f1ee2a585fdf36dca31fea05ee4c
Shawn Koh <shawnkoh@me.com>
2020-03-01 04:27:41 +0800

    Refactor

--- Stress/Systems/VisualSystem.swift
+++ Stress/Systems/VisualSystem.swift
@@ -10,21 +10,22 @@ import Foundation
 
 class VisualSystem: GKSystem {
     init(scene: GKScene) {
-        super.init(scene: scene, requiredComponents: [VisualComponent.self])
+        super.init(scene: scene,
+                   requiredComponents: [VisualComponent.self],
+                   excludedComponents: [DidAttachVisualComponent.self])
     }
 
     override func update(deltaTime: TimeInterval) {
-        entities.filter { $0.component(ofType: DidAttachVisualComponent.self ) == nil }
-                .forEach { entity in
-                    guard let view = entity.component(ofType: VisualComponent.self)?.view else {
-                        fatalError("Could not access visual component")
-                    }
-                    guard let parent = scene.view else {
-                        return
-                    }
+        entities.forEach { entity in
+            guard let view = entity.component(ofType: VisualComponent.self)?.view else {
+                fatalError("Could not access visual component")
+            }
+            guard let parent = scene.view else {
+                return
+            }
 
-                    parent.addSubview(view)
-                    entity.addComponent(DidAttachVisualComponent())
-                }
+            parent.addSubview(view)
+            entity.addComponent(DidAttachVisualComponent())
+        }
     }
 }
